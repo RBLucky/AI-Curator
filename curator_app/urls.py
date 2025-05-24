49ab776122961108c2faf9a3cc6e3dd7a0bddf4b
Lucky Nkosi <lucky.nkosi@ogilvy.co.za>
2025-05-24 15:35:01 +0200

    update internals

--- curator_app/urls.py
+++ curator_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('news/', views.news_list_view, name='news_list'),
     path('tools/', views.tool_list_view, name='tool_list'),
     path('tools/<int:pk>/', views.tool_detail_view, name='tool_detail'),
+    path(f'tasks/13b577f999959c93427a56706498d7ae8013520e/fetch-news/<str:secret>/', views.trigger_fetch_news_view, name='trigger_fetch_news'),
 ]
